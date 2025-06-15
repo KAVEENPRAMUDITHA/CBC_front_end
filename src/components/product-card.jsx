64e9e408f0c8c54db813591ebd4b4defacadbb81
Kaveen Pramuditha <pramudithakaveen51@gmail.com>
2025-06-16 01:51:50 +0530

    added a login page ✏️

---
+++ src/components/product-card.jsx
@@ -0,0 +1,11 @@
+export default function ProductCard() {
+
+    return (
+        <div>
+            <h1>Product Card</h1>
+            <p>Product description</p>
+            <p>Product price</p>
+            <button>Add to cart</button>
+        </div>
+    )
+}
---
+++ src/pages/loginPage.jsx
@@ -0,0 +1,13 @@
+export default function LoginPage() {
+
+    return (
+        <div className=" w-full h-screen bg-red-700  bg-[url('/loginbg.jpg')] bg-cover bg-center flex">
+            <div className="border w-[50%] h-full">
+
+            </div>
+            <div className="border  w-[50%] h-full">
+
+            </div>
+        </div>
+    )
+}
