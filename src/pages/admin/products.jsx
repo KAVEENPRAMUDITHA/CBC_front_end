8687f03015f7af992f467f9807e4f1480753388d
Kaveen Pramuditha <pramudithakaveen51@gmail.com>
2025-07-15 01:44:06 +0530

    ✏️

--- src/pages/admin/products.jsx
+++ src/pages/admin/products.jsx
@@ -5,21 +5,33 @@ export default function AdminProductsPage() {
 
     const [products, setProducts] = useState([]);
 
-    useEffect (() => {
+    useEffect(() => {
 
         axios.get(import.meta.env.VITE_BACKEND_URL + "/api/product")
             .then((response) => {
                 console.log("Products:", response.data);
                 setProducts(response.data);
             });
-    },[]);
-    
-    
+    }, []);
+
+
 
     return (
-        <div className="w-full h-full bg-red-500 rounded-lg flex items-center justify-center">
-            { 
-                
+        <div className="w-full h-full bg-red-500 rounded-lg">
+            {
+                products.map(
+                    (product, index) => (
+                        <div key={index} className="bg-white p-4 m-2 rounded shadow">
+                            <div className="flex justify-between items-center">
+                                <h2 className="text-xl font-bold">{product.name}</h2>
+                                <button className="bg-red-500 text-white px-2 py-1 rounded">Delete</button>
+                            </div>
+                            <div className="flex items-center">
+                                <p className="text-green-600">${product.price}</p>
+
+                            </div>
+                        </div>
+                    ))
             }
         </div>
     );
